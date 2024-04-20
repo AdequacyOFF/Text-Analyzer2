38e4a9d79b5bfb4ae4295dce78d3a8fac6e885ca
AdequacyOFF <adequacyoff-AO@yandex.ru>
2024-04-21 02:05:39 +0300

    dd

--- src/Pages/Document.jsx
+++ src/Pages/Document.jsx
@@ -1,13 +1,21 @@
 
 import '../Pages_css/Document.css';
+import DropFileInput from './DropFileInput';
 
 function Document() {
+
+  const onFileChange = (files) => {
+    console.log(files);
+}
 
   return (
     <div className='section'>
       <div className='answer-doc'>
 
       </div>
+      <div className='DropFileInput'>
+          <DropFileInput onFileChange={(files) => onFileChange(files)}/>
+    </div>
       <form action="/">
         <button className='btn-doc-doc btn-doc-text-doc'>ТЕКСТ</button>
       </form>
