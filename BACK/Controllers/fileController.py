3f4153cf860cbcc16af5b0e2fa78403cbefb5c1a
AdequacyOFF <adequacyoff-AO@yandex.ru>
2024-04-25 02:12:05 +0300

    1

--- BACK/Controllers/fileController.py
+++ BACK/Controllers/fileController.py
@@ -7,18 +7,14 @@ def allowed_file(filename, allowedExtensions):
     return '.' in filename and filename.rsplit('.', 1)[1] in allowedExtensions
 
 def file_process(uploadFolder, allowedExtensions):
-    print ("upload_file()")
     if request.method == 'POST':
         if 'files' not in request.files:
             return make_response("No file part", 507)
-        print(request.files)
         file = request.files['files']
-        print (file)
         if file.filename == '':
             return make_response("No selected file", 507)
         if file and allowed_file(file.filename, allowedExtensions):
             filename = secure_filename(file.filename)
-            print(filename)
             file.save(os.path.join(uploadFolder, filename))
             filepath = (uploadFolder +"\\"+ filename).replace('/','\\')
             s_file = open(filepath, encoding='utf-8')
